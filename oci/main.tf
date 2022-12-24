91c7274095ff519df8c3e70262f6e24b6116baa1
jess3jane <me@jess.coffee>
2022-12-23 23:46:12 -0800

    Add VCN

---
+++ oci/main.tf
@@ -0,0 +1,6 @@
+resource "oci_core_vcn" "vcn" {
+	compartment_id = var.compartment_id
+	cidr_blocks = ["172.16.0.0/20"]
+	display_name = "HomeLab VCN"
+	dns_label = "homelab"
+}
--- oci/provider.tf
+++ oci/provider.tf
@@ -1,5 +1,6 @@
-variable "tenancy_ocid" {}
+variable "compartment_oicd" {}
 variable "region" {}
+variable "tenancy_ocid" {}
 
 provider "oci" {
    tenancy_ocid = "${var.tenancy_ocid}"
