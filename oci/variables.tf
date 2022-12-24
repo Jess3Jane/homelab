448c07150855d822fa831cdf6da5f8a0e50c5993
jess3jane <me@jess.coffee>
2022-12-24 01:17:05 -0800

    Fix variables

--- oci/variables.tf
+++ oci/variables.tf
@@ -1,14 +1,14 @@
 variable "availability_domain" {
-	type = "string"
-	default_value = "jTyh:US-SANJOSE-1-AD-1"
+	type = string
+	default = "jTyh:US-SANJOSE-1-AD-1"
 }
 
 variable "amd_shape" {
-	type = "string"
-	default_value = "VM.Standard.E2.1.Micro"
+	type = string
+	default = "VM.Standard.E2.1.Micro"
 }
 
 variable "amd_image" {
-	type = "string"
-	default_value = "ocid1.tenancy.oc1..aaaaaaaa3wwekfseu6rn3oaqp4u4s3alfmemzqiighv4zsoumctw54jdvttq"
+	type = string
+	default = "ocid1.tenancy.oc1..aaaaaaaa3wwekfseu6rn3oaqp4u4s3alfmemzqiighv4zsoumctw54jdvttq"
 }
