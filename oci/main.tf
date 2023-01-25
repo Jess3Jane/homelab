d664a94cf71729905920c8a17e340386a12a3f5b
jess3jane <me@jess.coffee>
2023-01-24 22:30:06 -0800

    Try adding a subnet

--- oci/main.tf
+++ oci/main.tf
@@ -1,18 +1,26 @@
-#resource "oci_core_vcn" "vcn" {
-#	compartment_id = var.compartment_ocid
-#	cidr_blocks = ["172.16.0.0/20"]
-#	display_name = "HomeLab VCN"
-#	dns_label = "homelab"
-#}
+resource "oci_core_vcn" "vcn" {
+	compartment_id = var.compartment_ocid
+	cidr_blocks = ["172.16.0.0/20"]
+	display_name = "HomeLab VCN"
+	dns_label = "homelab"
+}
+
+resource "oci_core_subnet" "subnet" {
+	cidr_block = "172.16.0.0/20"
+	compartment_id = var.compartment_ocid
+	display_name "Homelab VCN Subnet"
+	vcn_id = oci_core_vcn.vcn.id
+}
 
 resource "oci_core_instance" "atropos" {
 	availability_domain = var.availability_domain
 	compartment_id = var.compartment_ocid
-	#create_vnic_details {
-	#	hostname_label = "a"
-	#	private_ip = "172.16.0.2"
-	#}
-	display_name = "atropos"
+	create_vnic_details {
+		hostname_label = "atropos"
+		private_ip = "172.16.0.2"
+		subnet_id = oci_core_subnet.subnet.id
+	}
+	display_name = "Atropos"
 	fault_domain = "FAULT-DOMAIN-1"
 	shape = var.amd_shape
 	source_details {
