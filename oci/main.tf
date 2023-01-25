85aebf82604a0298319f1af7e53c8f5bc69916d2
jess3jane <me@jess.coffee>
2023-01-24 22:22:34 -0800

    Remove vnic

--- oci/main.tf
+++ oci/main.tf
@@ -1,26 +1,18 @@
-resource "oci_core_vcn" "vcn" {
-	compartment_id = var.compartment_ocid
-	cidr_blocks = ["172.16.0.0/20"]
-	display_name = "HomeLab VCN"
-	dns_label = "homelab"
-}
+#resource "oci_core_vcn" "vcn" {
+#	compartment_id = var.compartment_ocid
+#	cidr_blocks = ["172.16.0.0/20"]
+#	display_name = "HomeLab VCN"
+#	dns_label = "homelab"
+#}
 
-resource "oci_core_vlan" "vlan" {
-	compartment_id = var.compartment_ocid
-	vcn_id = oci_core_vcn.vcn.id
-	cidr_block = "172.16.0.0/20"
-	availability_domain = var.availability_domain
-}
-
-resource "oci_core_instance" "instance_a" {
+resource "oci_core_instance" "atropos" {
 	availability_domain = var.availability_domain
 	compartment_id = var.compartment_ocid
-	create_vnic_details {
-		hostname_label = "a"
-		private_ip = "172.16.0.2"
-		vlan_id = oci_core_vlan.vlan.id
-	}
-	display_name = "instance-a"
+	#create_vnic_details {
+	#	hostname_label = "a"
+	#	private_ip = "172.16.0.2"
+	#}
+	display_name = "atropos"
 	fault_domain = "FAULT-DOMAIN-1"
 	shape = var.amd_shape
 	source_details {
