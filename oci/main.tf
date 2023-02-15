93e7c1e5e84f328474c67486b75751dc37b62349
jess3jane <me@jess.coffee>
2023-02-14 19:22:24 -0800

    Cleanup

--- oci/main.tf
+++ oci/main.tf
@@ -1,33 +1,33 @@
 resource "oci_core_vcn" "vcn" {
-	compartment_id = var.compartment_ocid
-	cidr_blocks = ["172.16.0.0/20"]
-	display_name = "HomeLab VCN"
-	dns_label = "homelab"
+  compartment_id = var.compartment_ocid
+  cidr_blocks = ["172.16.0.0/20"]
+  display_name = "HomeLab VCN"
+  dns_label = "homelab"
 }
 
 resource "oci_core_subnet" "subnet" {
-	cidr_block = "172.16.0.0/20"
-	compartment_id = var.compartment_ocid
-	display_name = "Homelab VCN Subnet"
-	vcn_id = oci_core_vcn.vcn.id
+  cidr_block = "172.16.0.0/20"
+  compartment_id = var.compartment_ocid
+  display_name = "Homelab VCN Subnet"
+  vcn_id = oci_core_vcn.vcn.id
 }
 
-resource "oci_core_instance" "atropos" {
-	availability_domain = var.availability_domain
-	compartment_id = var.compartment_ocid
-	create_vnic_details {
-		#hostname_label = "atropos"
-		private_ip = "172.16.0.2"
-		subnet_id = oci_core_subnet.subnet.id
-	}
-	display_name = "Atropos"
-	fault_domain = "FAULT-DOMAIN-1"
-	shape = var.amd_shape
-	source_details {
-		source_id = var.amd_image
-		source_type = "image"
-	}
-	extended_metadata = {
-		ssh_authorized_keys = var.public_key
-	}
+resource "oci_core_instance" "instances" {
+  for_each = var.instances
+  availability_domain = each.value.availability_domain
+  compartment_id = var.compartment_ocid
+  create_vnic_details {
+    private_ip = each.value.private_ip
+    subnet_id = oci_core_subnet.subnet.id
+  }
+  display_name = each.key
+  fault_domain = "FAULT-DOMAIN-1"
+  shape = each.value.shape
+  source_details {
+    source_id = each.value.image
+    source_type = "image"
+  }
+  extended_metadata = {
+    ssh_authorized_keys = var.public_key
+  }
 }
--- oci/variables.tf
+++ oci/variables.tf
@@ -1,19 +1,13 @@
-variable "availability_domain" {
-	type = string
-	default = "jTyh:US-SANJOSE-1-AD-1"
-}
-
-variable "amd_shape" {
-	type = string
-	default = "VM.Standard.E2.1.Micro"
-}
-
-variable "amd_image" {
-	type = string
-	default = "ocid1.image.oc1.us-sanjose-1.aaaaaaaaudhtosvzok3k6csi2cnhsee7gfezgnkd7gq742gsoe4xjaxiaoja"
-}
-
 variable "public_key" {
 	type = string
 	default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1eVH5tVioIhK7A295n1F4hPXpAlLpbXksfutuL4HUf1U4ZfchJmF4ujtiWyDhiIex+ZZMBJNwSeYEHU0Mv58zZ5GhhNJ1pZZ0PRo/sFliRgvgh609YyHmGJJ4tJ4n1gnmDBbR1fy+U7rz22JkGKRdLvJ/tLNfGrV9Wj415HHFKWjNBJ8f3A0npRVVkVZGZAuDWzeELevK/Dt77TCs4rSDTw4hYIHqLxJarrI346KkpSo0Ro55q2IiUX5eptyqYsqPLKpBce+TS0dqJiTrwwq8MmnvupfGPGZP7kGvmOLMsYBdP+R9jGV7dJvwo1BJhSaM1Zy2EbLQvcDjEoe9fnXrsbp4wexDetgkDvjQfP5yQFWCT6Bf0Rw+yUYcN/S/MNLexigmq+BWvhQ671csHXW7N9LdXIFGQcz4yrub03XSNlmFYuieYIPCwSTmOWYy7hfalVoQR8QI1jrHkgb79ZKoDNgeWHEfVoY+sOsVu+X7wGdY1C5hbl2OrIlITnOwCcMoEVQt9ZOUOQ+1nTKJFnWBzdcFyxrO5K2lUq2CsILXRZEZcd7YKcs1SZdM0F9GtVumSiI6HhazpAkjSaAlq8leKXjswHrBYFyYGwnOlAWkSFZeyPnf7q+BXkpiJ9NsgVfUEj8e+Z0nWCgtT/Ml4OcUuDx+BzfHN7kSRoz/IG3Ytw== me@jess.coffee"
+}
+
+variable "instances" {
+  type = map(object({
+    private_ip = string,
+    image = string,
+    shape = string,
+    availiblity_domain = string,
+  }))	
 }
